e0f6394557fafff30a00f6c7d190e44ea260eac0
DavidFreire-FEUP <up201806711@fe.up.pt>
2022-07-12 14:37:17 +0100

    Relu impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+
+fn relu(input: f32) -> f32{
+    if input>1.0 {
+        return 1.0
+    } 
+    else if input<0.0 {
+        return 0.0;
+    }
+    return input;
+}
+
 fn main() {
     println!("Hello, world!");
 }
